28690835df8b73b71e93c7106031ac45709ad337
timste <ts2701@googlemail.com>
2013-07-05 16:09:32 +0200

    comments

--- app/code/community/Timste/Bodyclass/Block/Html.php
+++ app/code/community/Timste/Bodyclass/Block/Html.php
@@ -1,101 +1,109 @@
 <?php
 
+/**
+ *
+ */
 class Timste_Bodyclass_Block_Html extends Mage_Page_Block_Html
 {
+    /**
+     * @var string
+     */
     protected $_useragent = '';
 
     /**
      * Operating systems (check Windows CE before Windows and Android before Linux!)
      */
-    public $timste_os = array
-        (
-            'Macintosh'     => array('os'=>'mac',        'mobile'=>false),
-            'Windows CE'    => array('os'=>'win-ce',     'mobile'=>true),
-            'Windows Phone' => array('os'=>'win-ce',     'mobile'=>true),
-            'Windows'       => array('os'=>'win',        'mobile'=>false),
-            'iPad'          => array('os'=>'ios',        'mobile'=>false),
-            'iPhone'        => array('os'=>'ios',        'mobile'=>true),
-            'iPod'          => array('os'=>'ios',        'mobile'=>true),
-            'Android'       => array('os'=>'android',    'mobile'=>true),
-            'BB10'          => array('os'=>'blackberry', 'mobile'=>true),
-            'Blackberry'    => array('os'=>'blackberry', 'mobile'=>true),
-            'Symbian'       => array('os'=>'symbian',    'mobile'=>true),
-            'WebOS'         => array('os'=>'webos',      'mobile'=>true),
-            'Linux'         => array('os'=>'unix',       'mobile'=>false),
-            'FreeBSD'       => array('os'=>'unix',       'mobile'=>false),
-            'OpenBSD'       => array('os'=>'unix',       'mobile'=>false),
-            'NetBSD'        => array('os'=>'unix',       'mobile'=>false),
-        );
+    public $timsteOs = array
+    (
+        'Macintosh'     => array('os'=>'mac',        'mobile'=>false),
+        'Windows CE'    => array('os'=>'win-ce',     'mobile'=>true),
+        'Windows Phone' => array('os'=>'win-ce',     'mobile'=>true),
+        'Windows'       => array('os'=>'win',        'mobile'=>false),
+        'iPad'          => array('os'=>'ios',        'mobile'=>false),
+        'iPhone'        => array('os'=>'ios',        'mobile'=>true),
+        'iPod'          => array('os'=>'ios',        'mobile'=>true),
+        'Android'       => array('os'=>'android',    'mobile'=>true),
+        'BB10'          => array('os'=>'blackberry', 'mobile'=>true),
+        'Blackberry'    => array('os'=>'blackberry', 'mobile'=>true),
+        'Symbian'       => array('os'=>'symbian',    'mobile'=>true),
+        'WebOS'         => array('os'=>'webos',      'mobile'=>true),
+        'Linux'         => array('os'=>'unix',       'mobile'=>false),
+        'FreeBSD'       => array('os'=>'unix',       'mobile'=>false),
+        'OpenBSD'       => array('os'=>'unix',       'mobile'=>false),
+        'NetBSD'        => array('os'=>'unix',       'mobile'=>false),
+    );
 
-        /**
-         * Browsers (check OmniWeb and Silk before Safari and Opera Mini/Mobi before Opera!)
-         */
-        public $timste_browser = array
-        (
-            'MSIE'       => array(  'browser'=>'ie',           
-                                    'shorty'=>'ie', 
-                                    'engine'=>'trident', 
-                                    'version'=>'/^.*?MSIE (\d+(\.\d+)*).*$/'
-                                    ),
-            'Firefox'    => array(  'browser'=>'firefox',      
-                                    'shorty'=>'fx', 
-                                    'engine'=>'gecko',   
-                                    'version'=>'/^.*Firefox\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Chrome'     => array(  'browser'=>'chrome',       
-                                    'shorty'=>'ch', 
-                                    'engine'=>'webkit',  
-                                    'version'=>'/^.*Chrome\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'OmniWeb'    => array(  'browser'=>'omniweb',      
-                                    'shorty'=>'ow', 
-                                    'engine'=>'webkit',  
-                                    'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Silk'       => array(  'browser'=>'silk',         
-                                    'shorty'=>'si', 
-                                    'engine'=>'silk',    
-                                    'version'=>'/^.*Silk\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Safari'     => array(  'browser'=>'safari',       
-                                    'shorty'=>'sf', 
-                                    'engine'=>'webkit',  
-                                    'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Opera Mini' => array(  'browser'=>'opera-mini',   
-                                    'shorty'=>'oi', 
-                                    'engine'=>'presto',  
-                                    'version'=>'/^.*Opera Mini\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Opera Mobi' => array(  'browser'=>'opera-mobile', 
-                                    'shorty'=>'om', 
-                                    'engine'=>'presto',  
-                                    'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Opera'      => array(  'browser'=>'opera',        
-                                    'shorty'=>'op', 
-                                    'engine'=>'presto',  
-                                    'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'IEMobile'   => array(  'browser'=>'ie-mobile',    
-                                    'shorty'=>'im', 
-                                    'engine'=>'trident', 
-                                    'version'=>'/^.*IEMobile (\d+(\.\d+)*).*$/'
-                                    ),
-            'Camino'     => array(  'browser'=>'camino',       
-                                    'shorty'=>'ca', 
-                                    'engine'=>'gecko',   
-                                    'version'=>'/^.*Camino\/(\d+(\.\d+)*).*$/'
-                                    ),
-            'Konqueror'  => array(  'browser'=>'konqueror',    
-                                    'shorty'=>'ko', 
-                                    'engine'=>'webkit',  
-                                    'version'=>'/^.*Konqueror\/(\d+(\.\d+)*).*$/'
-                                    )
-        );
+    /**
+     * Browsers (check OmniWeb and Silk before Safari and Opera Mini/Mobi before Opera!)
+     */
+    public $timsteBrowser = array
+    (
+        'MSIE'       => array(  'browser'=>'ie',
+            'shorty'=>'ie',
+            'engine'=>'trident',
+            'version'=>'/^.*?MSIE (\d+(\.\d+)*).*$/'
+        ),
+        'Firefox'    => array(  'browser'=>'firefox',
+            'shorty'=>'fx',
+            'engine'=>'gecko',
+            'version'=>'/^.*Firefox\/(\d+(\.\d+)*).*$/'
+        ),
+        'Chrome'     => array(  'browser'=>'chrome',
+            'shorty'=>'ch',
+            'engine'=>'webkit',
+            'version'=>'/^.*Chrome\/(\d+(\.\d+)*).*$/'
+        ),
+        'OmniWeb'    => array(  'browser'=>'omniweb',
+            'shorty'=>'ow',
+            'engine'=>'webkit',
+            'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
+        ),
+        'Silk'       => array(  'browser'=>'silk',
+            'shorty'=>'si',
+            'engine'=>'silk',
+            'version'=>'/^.*Silk\/(\d+(\.\d+)*).*$/'
+        ),
+        'Safari'     => array(  'browser'=>'safari',
+            'shorty'=>'sf',
+            'engine'=>'webkit',
+            'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
+        ),
+        'Opera Mini' => array(  'browser'=>'opera-mini',
+            'shorty'=>'oi',
+            'engine'=>'presto',
+            'version'=>'/^.*Opera Mini\/(\d+(\.\d+)*).*$/'
+        ),
+        'Opera Mobi' => array(  'browser'=>'opera-mobile',
+            'shorty'=>'om',
+            'engine'=>'presto',
+            'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
+        ),
+        'Opera'      => array(  'browser'=>'opera',
+            'shorty'=>'op',
+            'engine'=>'presto',
+            'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'
+        ),
+        'IEMobile'   => array(  'browser'=>'ie-mobile',
+            'shorty'=>'im',
+            'engine'=>'trident',
+            'version'=>'/^.*IEMobile (\d+(\.\d+)*).*$/'
+        ),
+        'Camino'     => array(  'browser'=>'camino',
+            'shorty'=>'ca',
+            'engine'=>'gecko',
+            'version'=>'/^.*Camino\/(\d+(\.\d+)*).*$/'
+        ),
+        'Konqueror'  => array(  'browser'=>'konqueror',
+            'shorty'=>'ko',
+            'engine'=>'webkit',
+            'version'=>'/^.*Konqueror\/(\d+(\.\d+)*).*$/'
+        )
+    );
 
 
-
+    /**
+     *
+     */
     public function __construct()
     {
         parent::__construct();
@@ -113,16 +121,18 @@ class Timste_Bodyclass_Block_Html extends Mage_Page_Block_Html
 
         $result = $this->agent();
 
-        $this->addBodyClass( $result['class'] );
-
-        if( $result['mobile'] == 1 ){
-            $this->addBodyClass( 'mobile' );
-        }
+        $this->addBodyClass( $result['os'] );
+        $this->addBodyClass( $result['browser'] );
+        $this->addBodyClass( $result['version'] );
+        $this->addBodyClass( $result['engine'] );
 
         $this->_beforeCacheUrl();
     }
 
 
+    /**
+     * @return array|string
+     */
     public function agent()
     {
 
@@ -139,7 +149,7 @@ class Timste_Bodyclass_Block_Html extends Mage_Page_Block_Html
         $engine = '';
 
         // Operating system
-        foreach ($this->timste_os as $k=>$v)
+        foreach ($this->timsteOs as $k=>$v)
         {
             if (stripos($ua, $k) !== false)
             {
@@ -152,7 +162,7 @@ class Timste_Bodyclass_Block_Html extends Mage_Page_Block_Html
         $return['os'] = $os;
 
         // Browser and version
-        foreach ($this->timste_browser as $k=>$v)
+        foreach ($this->timsteBrowser as $k=>$v)
         {
             if (stripos($ua, $k) !== false)
             {
@@ -181,7 +191,7 @@ class Timste_Bodyclass_Block_Html extends Mage_Page_Block_Html
             $return['class'] .= ' mobile';
         }
 
-        // Android tablets are not mobile (see #4150)
+        // Android tablets are not mobile
         if ($os == 'Android' && stripos('mobile', $ua) === false)
         {
             $mobile = false;
